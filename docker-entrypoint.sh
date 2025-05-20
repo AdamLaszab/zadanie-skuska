e98f13c7d91f9ce21fd6746356ce9ad3972bf841
Tamara <tamara.vachanova@gmail.com>
2025-05-20 19:03:59 +0200

    docker bugs resolved and added docker for nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,14 +9,9 @@ php artisan package:discover --ansi
 
 php artisan migrate --force
 
-echo "Checking if seeding is necessary..."
-if php artisan tinker --execute='echo \App\Models\User::where("email", "admin@example.com")->exists();' | grep -q "true"; then
-    echo "Admin user (admin@example.com) already exists. Skipping RolesAndPermissionsSeeder."
-else
-    echo "Admin user (admin@example.com) not found. Running RolesAndPermissionsSeeder..."
-    php artisan db:seed --class=RolesAndPermissionsSeeder --force
-    echo "RolesAndPermissionsSeeder completed."
-fi
+echo "Running RolesAndPermissionsSeeder..."
+php artisan db:seed --class=RolesAndPermissionsSeeder --force
+echo "RolesAndPermissionsSeeder completed."
 
 if [ -f ".env" ] && ! grep -q "^APP_KEY=.\+" ".env"; then
   echo "APP_KEY not found or empty in .env, generating..."
